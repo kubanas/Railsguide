570ab8b9636ba812a9a2198e3c21adf09141c1da
Kuba Sitko <jakobsitko.js@googlemail.com>
2017-08-22 15:52:49 +0200

    Added signup  users from UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] ="Welcome to the Devblog, Mister #{@user.username}"
+      redirect_to articles_path #articles_listing
+    else
+      render "new"
+    end
+
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-root to: "pages#home"
-get "/about", to: "pages#about"
-resources :articles
+  root to: "pages#home"
+  get "/about", to: "pages#about"
+  resources :articles
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
 
 
 
